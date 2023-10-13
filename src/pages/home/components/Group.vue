71a7522b88b38c6742743260379bc2827190b2f3
彭铮 <973020019@qq.com>
2023-10-13 17:07:47 +0800

    修改架子

--- src/pages/home/components/Group.vue
+++ src/pages/home/components/Group.vue
@@ -35,18 +35,8 @@
               </view>
             </view>
 
-            <view class="flex justify-start ml-40rpx h-146rpx">
-              <!-- #ifdef MP-WEIXIN -->
-              <ucharts-area
-                :height="146"
-                :width="226"
-                :loading="loading"
-                :is-show-tool-tip="false"
-                canvas-id="uchartsArea"
-                :color="['#F80000']"
-                :chart-data="chartDataArea"
-              />
-              <!-- #endif -->
+            <view class="flex justify-start ml-40rpx charts-box">
+              <qiun-data-charts type="area" :opts="opts" :chart-data="chartData" />
             </view>
           </view>
         </view>
@@ -71,18 +61,8 @@
               </view>
             </view>
 
-            <view class="flex justify-start ml-40rpx h-146rpx">
-              <!-- #ifdef MP-WEIXIN -->
-              <ucharts-area
-                :height="146"
-                :width="226"
-                :loading="loading"
-                :is-show-tool-tip="false"
-                canvas-id="uchartsArea"
-                :color="['#1A916D']"
-                :chart-data="chartDataArea"
-              />
-              <!-- #endif -->
+            <view class="flex justify-start ml-40rpx charts-box">
+              <qiun-data-charts type="area" :opts="opts" :chart-data="chartData" />
             </view>
           </view>
         </view>
@@ -92,10 +72,7 @@
 </template>
 
 <script>
-import UchartsArea from '@/components/ucharts/UchartsArea';
-
 export default {
-  components: { UchartsArea },
   data() {
     return {
       stockSelected: true,
@@ -103,6 +80,131 @@ export default {
       isRise: true,
       isDrop: false,
       loading: false,
+      chartData: {},
+      opts: {
+        timing: 'easeOut',
+        duration: 1000,
+        rotate: false,
+        rotateLock: false,
+        background: '#FFFFFF',
+        color: this.color,
+        padding: [0, 0, 0, 0],
+        fontSize: 13,
+        fontColor: '#666666',
+        dataLabel: false,
+        dataPointShape: false,
+        dataPointShapeType: 'solid',
+        touchMoveLimit: 60,
+        enableScroll: false,
+        enableMarkLine: false,
+        legend: {
+          show: false,
+          position: 'bottom',
+          float: 'center',
+          padding: 5,
+          margin: 5,
+          backgroundColor: 'rgba(0,0,0,0)',
+          borderColor: 'rgba(0,0,0,0)',
+          borderWidth: 0,
+          fontSize: 13,
+          fontColor: '#666666',
+          lineHeight: 11,
+          hiddenColor: '#CECECE',
+          itemGap: 10
+        },
+        xAxis: {
+          disableGrid: true,
+          disabled: true,
+          axisLine: false,
+          axisLineColor: '#CCCCCC',
+          calibration: false,
+          fontColor: '#666666',
+          fontSize: 13,
+          rotateLabel: false,
+          rotateAngle: 45,
+          itemCount: 5,
+          boundaryGap: 'center',
+          splitNumber: 5,
+          gridColor: '#CCCCCC',
+          gridType: 'solid',
+          dashLength: 4,
+          gridEval: 1,
+          scrollShow: false,
+          scrollAlign: 'left',
+          scrollColor: '#A6A6A6',
+          scrollBackgroundColor: '#EFEBEF',
+          formatter: ''
+        },
+        yAxis: {
+          gridType: 'dash',
+          dashLength: 2,
+          disabled: true,
+          disableGrid: true,
+          splitNumber: 5,
+          gridColor: '#CCCCCC',
+          padding: 10,
+          showTitle: false,
+          data: [
+            {
+              type: 'value',
+              position: 'left',
+              disabled: false,
+              axisLine: true,
+              axisLineColor: '#CCCCCC',
+              calibration: false,
+              fontColor: '#666666',
+              fontSize: 13,
+              textAlign: 'right',
+              title: '',
+              titleFontSize: 13,
+              titleOffsetY: 0,
+              titleOffsetX: 0,
+              titleFontColor: '#666666',
+              min: null,
+              max: null,
+              tofix: null,
+              unit: '',
+              formatter: ''
+            }
+          ]
+        },
+        extra: {
+          area: {
+            type: 'straight',
+            opacity: 0.2,
+            addLine: true,
+            width: 2,
+            gradient: true
+          },
+          tooltip: {
+            showBox: false,
+            showArrow: false,
+            showCategory: false,
+            borderWidth: 0,
+            borderRadius: 0,
+            borderColor: '#000000',
+            borderOpacity: 0.7,
+            bgColor: '#000000',
+            bgOpacity: 0.7,
+            gridType: 'solid',
+            dashLength: 4,
+            gridColor: '#CCCCCC',
+            fontColor: '#FFFFFF',
+            splitLine: true,
+            horizentalLine: false,
+            xAxisLabel: false,
+            yAxisLabel: false,
+            labelBgColor: '#FFFFFF',
+            labelBgOpacity: 0.7,
+            labelFontColor: '#666666'
+          },
+          markLine: {
+            type: 'solid',
+            dashLength: 4,
+            data: []
+          }
+        }
+      },
       chartDataArea: {
         categories: ['2016', '2017', '2018', '2019', '2020', '2021'],
         series: [
@@ -114,10 +216,34 @@ export default {
       }
     };
   },
-  onReady() {
-    this.loading = true;
+  created() {
+    this.getServerData();
   },
   methods: {
+    getServerData() {
+      // 模拟从服务器获取数据时的延时
+      setTimeout(() => {
+        // 模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
+        const res = {
+          categories: ['2018', '2019', '2020', '2021', '2022', '2023'],
+          series: [
+            {
+              name: '成交量A',
+              data: [35, 8, 25, 37, 4, 20]
+            },
+            {
+              name: '成交量B',
+              data: [70, 40, 65, 100, 44, 68]
+            },
+            {
+              name: '成交量C',
+              data: [100, 80, 95, 150, 112, 132]
+            }
+          ]
+        };
+        this.chartData = JSON.parse(JSON.stringify(res));
+      }, 500);
+    },
     clickStock() {
       this.stockSelected = true;
       this.fundSelected = false;
@@ -149,5 +275,10 @@ export default {
   background: linear-gradient(88deg, #effaf5 0%, #feffff 32%, #ffffff 100%);
   box-shadow: 1px 1px 1px 1px rgba(136, 136, 136, 0.16);
   border: 1px solid #e3e3e3;
+}
+
+.charts-box {
+  width: 200rpx;
+  height: 146rpx;
 }
 </style>
